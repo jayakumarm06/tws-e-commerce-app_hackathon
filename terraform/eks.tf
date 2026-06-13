1f9c5610883505a905225ffa0d3dee39edb4d26d
Jai Kumarm <jaikumarm061286@gmail.com>
2026-06-13 18:13:34 +0530

    modified desired level

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -88,9 +88,9 @@ module "eks" {
   eks_managed_node_groups = {
 
     tws-demo-ng = {
-      min_size     = 3
-      max_size     = 3
-      desired_size = 1
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
 
       instance_types = ["t3.medium"]
       capacity_type  = "SPOT"
